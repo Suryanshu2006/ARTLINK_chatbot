3d585316942d29d835e3d0f20017f100bc13d52a
Suryanshu Kumar Singh <86818999+Suryanshu2006@users.noreply.github.com>
2025-07-14 01:37:47 +0530

    Update chatbot.js

--- chatbot.js
+++ chatbot.js
@@ -26,14 +26,24 @@ const config = {
   responseMimeType: 'text/plain',
 };
 
-// CORS middleware
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://artlinkme.netlify.app'
+];
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
   }
+
   next();
 });
 
